060e18099a7b7069bb3f17c86b4201162222df8b
Boato Andrea <163646394+aandrea-boatoo@users.noreply.github.com>
2024-11-13 18:45:10 +0100

    uncomplete js form data

--- script/script.js
+++ script/script.js
@@ -2,35 +2,38 @@
 // CARD MEMBRI GIA ESISTENTI
 import { teamMembers } from "./teamMembers.js";
 
-const staffSection =document.getElementById("staff");
+const staffSection = document.getElementById("staff");
 
-for(let value of teamMembers){
+for (let value of teamMembers) {
     const template = `
-    <div class="card d-flex flex-row justify-content-between">
+    <div class="card d-flex  flex-row justify-content-between">
     <div class="image">
         <img src="${value.img}" alt="${value.name}">
     </div>
     <div class="info">
         <h5 class="name">${value.name}</h5>
         <p class="role">${value.role}</p>
-        <p class="email">${value.email}</p>
     </div>
-</div>` 
-staffSection.innerHTML += template;
+    <p class="email">${value.email}</p>
+</div>`
+    staffSection.innerHTML += template;
 }
 
 // FORM E INSERIMENTO NUOVO MEMBRO
 const nameInput = document.getElementById("inpName");
-const surnameInput = document.getElementById("inpSurname");
 const jobInput = document.getElementById("inpJob");
 const emailInput = document.getElementById("inpEmail");
 const imgInput = document.getElementById("inpImg");
 const subButton = document.getElementById("subButton");
 
-subButton.addEventListener('click', newMember(event));
-
-
-function newMember(event){
-    event.pre
-
-}
+subButton.addEventListener('click', function () {
+    event.preventDefault();
+    const newMember = {
+        name: nameInput.value,
+        role: jobInput.value,
+        email: emailInput.value,
+        img: "img/male2.png",
+    }
+    teamMembers.push(newMember);
+    console.log(teamMembers);
+});
